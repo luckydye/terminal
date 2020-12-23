a58c9b7425135937827615ae6d294aac6884f50b
Tim Havlicek <tim.h4vlicek@gmail.com>
2020-12-23 23:20:20 +0100

    chat

--- app/server.js
+++ app/server.js
@@ -22,7 +22,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('src'));
+app.get('/chat', express.static('src'));
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
