fbf2b63019a55e3a866f25b491aa4e3e65e1bb4f
Tim Havlicek <tim.h4vlicek@gmail.com>
2020-12-24 02:12:22 +0100

    javascript-obfuscator

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const PACKAGE = require("./package.json");
+const path = require("path");
+const webpack = require("webpack");
+const WebpackObfuscator = require('webpack-obfuscator');
+
+module.exports = {
+    mode: "development",
+    entry: {
+        main: "./src/main.js",
+    },
+    output: {
+        globalObject: "self",
+        filename: "[name].min.js",
+        path: path.resolve(__dirname, "public"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: [ 
+                    path.resolve(__dirname, 'excluded_file_name.js') 
+                ],
+                enforce: 'post',
+                use: { 
+                    loader: WebpackObfuscator.loader, 
+                    options: {
+                        rotateStringArray: true
+                    }
+                }
+            }
+        ]
+    }
+};
